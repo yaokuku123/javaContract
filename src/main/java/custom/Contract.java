ce8dd6311e739916a821cd8a44d054e12cc8f6aa
qinbohan@ <qinbohan@126.com>
2019-10-17 19:48:47 +0800

    演示用程序

--- src/main/java/custom/Contract.java
+++ src/main/java/custom/Contract.java
@@ -1,27 +1,17 @@
 package custom;
 
-import java.io.Serializable;
+import shellChain.ShellChainContract;
 
-public class Contract implements Serializable{
+
+public class Contract extends  ShellChainContract {
 
     private String name;
     private int times;
 
     public Contract(String name) {
         super();
-        //给参数赋值
         this.name = name;
         this.times = 0;
-    }
-
-    //之后执行时直接用上次之序列化返回的值进行反序列化，执行完后在此序列化
-    public String addOne(String name){
-        if(name.equals(this.name)) {
-            times += 1;
-            return "sucess " + times;
-        }
-        else
-            return "access denied with name " + name;
     }
 
     public String addT(String name, int T){
---
+++ src/main/java/shellChain/ShellChainContract.java
@@ -0,0 +1,9 @@
+package shellChain;
+
+import java.io.Serializable;
+
+/**
+ * Created by lenovo on 2019/10/17.
+ */
+public class ShellChainContract implements Serializable {
+}
